37eb22ea96a7613047b6e1fc26865dafe6249ae9
Ruben Almendares <camiloalmendarez47@gmail.com>
2023-06-10 09:01:05 -0600

    Agregada sección About Me y corregio encapsulación de hijos

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,13 +7,15 @@ const Body = () => {
 
     <section className='h-5/6 bg-gray-900 my-16'>
 
+        {/*HI!*/}
         <div className='flex px-5 py-12 justify-between mt-5'>
 
-            <div className="text-white px-20 text-5xl w-3/5 flex flex-col justify-center text-center">
+            {/*Who Am I*/}
+            <div className="text-white px-20 text-5xl w-8/12 flex flex-col justify-center text-justify">
 
               ¡Hey!, I'm Ruben Almendares
 
-              <div className="text-lg mt-5 text-center">
+              <div className="text-lg mt-5">
                 Ingeniero en Sistemas con experiencia en el {""}
                 <span className='text-red-500 underline decoration-1'>Desarrollo Frontend</span> enfocado al diseño y desarrollo
                 de sitios web bajo los mejores estándares y mediante diversas tecnologías para asegurar excelentes y funcionales resultados.
@@ -21,15 +23,36 @@ const Body = () => {
 
             </div>
 
-            <div className='w-3/12 flex flex-col justify-center'>
-              <div className='flex flex-col border-x-2 border-x-red-600 border-dashed rounded-full'>
+            {/*Profile Pic*/}
+            <div className='w-3/12 flex flex-col mr-5 justify-center'>
+              <div className='flex flex-col w-full border-x-2 border-x-red-600 border-dashed rounded-full'>
                     <img src="/personalContent/ej_fotoPersonal.jpeg" className=" rounded-full"/>
               </div>
             </div>
         </div>
 
+        {/*ABOUT ME*/}
         <Divisor/>
-        <Tabs/>
+        <section className='flex justify-between'>
+
+          <div className='w-5/12'>
+
+          </div>
+          
+          {/*Talking about me*/}
+          <div className='w-7/12 px-5 text-white text-justify'>
+            <p className='uppercase text-3xl'>About me</p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt laborum magni distinctio, repudiandae consequatur quis voluptatum voluptatem? A eius vitae omnis aliquid reiciendis, molestiae voluptatum aliquam maxime consequatur reprehenderit perferendis!
+          </div>
+        </section>
+
+        {/*RESUME OF PROJECTS*/}
+        <Divisor/>
+        <section className='flex flex-col'>
+          <span className='text-white uppercase mx-auto py-2 text-3xl'>Short Resume of my Projects</span>
+          <Tabs/>
+        </section>
+        
 
     </section>
   )
