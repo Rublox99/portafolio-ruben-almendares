ee429dd0b4e456e031704d3b291dabb37dc3e80d
Ruben Almendares <camiloalmendarez47@gmail.com>
2023-06-10 09:38:56 -0600

    Agregada función que edita lo visual al desplazar el scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,38 @@
 import React from 'react'
 import { useState } from 'react'
-import { useEffect } from 'react';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 const Header = () => {
-  const [posicionScrollY, setPosicionScrollY]= useState(0);
+
+  const [movimiento, setMovimiento]= useState(false);
   
   //Cambio visual en el navbar al hacer scroll
   window.onscroll = () => {
     const posicionY= window.scrollY;
-    setPosicionScrollY(posicionY);
-  }
 
-  useEffect( () => {
-    if (posicionScrollY> 0){
-      //console.log(posicionScrollY)
+    if(posicionY>= 50){
+      setMovimiento(true)
+    } else{
+      setMovimiento(false)
     }
-  }, [posicionScrollY])
+  }
   //------------------------------------------
   
   return (
 
     <div className='bg-gray-900 relative'>
         
-        <nav className='px-5 py-5 w-full text-white bg-gray-900 font-medium text-lg flex uppercase items-center fixed top-0'>
+        <nav className= {(classNames("px-5 py-5 w-full text-white font-medium text-lg flex uppercase items-center fixed top-0 transition-all duration-500",
+        movimiento ? classNames("bg-red-800") : classNames("bg-gray-900")))}>
           <span className='w-2/6 text-xl normal-case left-0'>Done with React & Vite</span>
 
           <ul className='w-4/6 text-white uppercase flex justify-end'>
-            <li className='mx-5 hover:text-red-500 transition-all'><a href="">Home</a></li>|
-            <li className='mx-5 hover:text-red-500 transition-all'><a href="">My knowledge </a></li>|
-            <li className='mx-5 hover:text-red-500 transition-all'><a href="">¿How to contact me?</a></li>
+            <li className={movimiento ? classNames("mx-5 hover:text-gray-900 transition-all") : classNames("mx-5 hover:text-red-800 transition-all")}><a href="">Home</a></li>|
+            <li className={movimiento ? classNames("mx-5 hover:text-gray-900 transition-all") : classNames("mx-5 hover:text-red-800 transition-all")}><a href="">My knowledge </a></li>|
+            <li className={movimiento ? classNames("mx-5 hover:text-gray-900 transition-all") : classNames("mx-5 hover:text-red-800 transition-all")}><a href="">¿How to contact me?</a></li>
           </ul>
         </nav>
 
