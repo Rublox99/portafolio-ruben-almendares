0c8e0cc28733e62808f6e94abea024784764049f
Ruben Almendares <camiloalmendarez47@gmail.com>
2023-06-09 18:07:14 -0600

    Modificación del botón Enviar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 /*bg-svg1 bg-center bg-cover*/
 const Footer = () => {
   return (
-    <section className='h-2/6 w-full bg-gray-900 text-white flex justify-center'>
+    <section className='h-2/6 py-2 w-full bg-gray-900 text-white flex justify-center'>
       <section className='flex flex-col justify-center w-2/6'>
           <label className='font-normal mx-auto'>I'm open to criticism for improvement</label>
           <textarea name="Comentario" id="idComentario" className='text-black rounded-md resize-none'></textarea>
-          <input type="button" value="Enviar" className='text-center mt-1 rounded-md bg-red-700 cursor-pointer hover:bg-red-500 transition-all'/>
+          <input type="button" value="Enviar" className='text-center mt-3 rounded-sm bg-red-700 cursor-pointer hover:bg-red-500 transition-all'/>
       </section>
     </section>
   )
