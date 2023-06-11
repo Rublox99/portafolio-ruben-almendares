d2a680b68456bf7e0375e19901b43614149f5af9
Ruben Almendares <camiloalmendarez47@gmail.com>
2023-06-10 19:25:06 -0600

    Componentes con redireccionamiento como páginas gracias a React Router

---
+++ src/pages/ContactMe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const ContactMe = () => {
+  return (
+    <div className='bg-gray-900 flex flex-col h-screen'>
+      <Header/>
+      <section className='mb-auto'></section>
+      <Footer/>
+    </div>
+  )
+}
+
+export default ContactMe
---
+++ src/pages/MyKnowledge.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const MyKnowledge = () => {
+  return (
+    <section className='bg-gray-900 flex flex-col h-screen'>
+      <Header/>
+      <section className='mb-auto'></section>
+      <Footer/>
+    </section>
+  )
+}
+
+export default MyKnowledge
