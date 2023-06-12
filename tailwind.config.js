12fe9f0cb244ddceb302c06e2afccab96e1cbc14
Ruben Almendares <camiloalmendarez47@gmail.com>
2023-06-11 18:49:01 -0600

    Configuraciones creadas para tamaño responsivo, phone y tablet

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ export default {
         'svg1': "url('src/graphics/webGraphics/slanted-gradient.svg')"
       }
     },
+    screens: {
+      'tablet': {'max': '1024px'}, //@media (max-width: 768px)
+      'phone': {'max': '480px'}
+    }
   },
   plugins: [],
 
