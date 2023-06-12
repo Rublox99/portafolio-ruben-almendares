6e9f8c0ed896299b2d7aed130f7fe43e3a25267d
Ruben Almendares <camiloalmendarez47@gmail.com>
2023-06-12 15:38:30 -0600

    Responsividad funcional - Phone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,16 +34,31 @@ const Header = () => {
 
     <div className='bg-gray-900 '>
         
-        {/*BUTTON para Responsive Phone*/}
-        <div alt="navbar-button" className='hidden phone:flex phone:fixed w-full h-14 bg-red-800'>
-          <span className= {(classNames('mr-auto my-auto px-1 text-white text-xl'))}>Made with React & Vite</span>
-          <img src="webGraphics/navbar-icon.svg" onClick={navbarExtend} alt="navbar-icon" className='h-14 absolute right-0 p-1 active:scale-90 transition-all'/>
+        {/*BUTTON para Responsive Navbar Phone*/}
+        <div alt="navbar-button" className={(movimiento ? 
+        'hidden phone:flex phone:fixed w-full h-14 bg-gray-900 transition-all duration-300' 
+        : 
+        'hidden phone:flex phone:fixed w-full h-14 bg-red-800 transition-all duration-300')}>
+          <span className= {navbar ? classNames("hidden") : classNames("w-full mr-auto my-auto px-3 text-white text-xl")}>
+            Made with React & Vite
+          </span>
+          <div className='flex flex-col w-full absolute'>
+            <ul className={( 
+              navbar ? 
+              classNames('w-full py-2 text-white uppercase flex justify-center flex-col mx-auto text-center bg-red-800 visible transition-all duration-100')
+              :
+              classNames("flex-row text-center transition-all invisible opacity-0 ease-in duration-75"))}>
+              <li className="mx-5 active:font-bold transition-all duration-200"><a href="/">Home</a></li><span className='phone:hidden'>|</span>
+              <li className="mx-5 active:font-bold transition-all duration-200"><a href="/myKnowledge">My knowledge </a></li><span className='phone:hidden'>|</span>
+              <li className="mx-5 active:font-bold transition-all duration-200"><a href="/contactMe">¿How to contact me?</a></li>
+            </ul>
+          </div>
+          <img src="webGraphics/navbar-icon.svg" onClick={navbarExtend} alt="navbar-icon" className='h-14 w-1/4 absolute right-0 p-1 active:scale-90 animate-pulse transition-all'/>
         </div>
        
-       {/*NAVBAR CON RESPONSIVE*/}
-        <nav className= {(classNames("px-5 py-5 w-full text-lg font-semibold text-white flex items-center fixed transition-all duration-500",
-        movimiento ? classNames("bg-red-800") : classNames("bg-gray-900")),
-        navbar ? classNames("phone:fixed phone:w-full phone:mt-0 phone:bg-red-800") : classNames("phone:hidden"))}>
+       {/*NAVBAR 1024px*/}
+        <nav className= {(classNames("px-5 py-5 w-full text-lg font-semibold text-white phone:hidden flex items-center fixed transition-all duration-500",
+        movimiento ? classNames("bg-red-800") : classNames("bg-gray-900")))}>
           <span className='w-2/6 text-xl left-0 phone:hidden flex'>Made with React & Vite</span>
 
           <ul className='w-4/6 text-white uppercase flex justify-end phone:justify-center phone:w-full phone:flex-col phone:text-center'>
