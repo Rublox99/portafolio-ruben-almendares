20053d4b2ee6660f8a3336aa86743400d69cdd23
Ruben Almendares <camiloalmendarez47@gmail.com>
2023-06-16 12:12:36 -0600

    Creación de atajo a cv

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -25,9 +25,15 @@ const Body = () => {
             </div>
 
             {/*Profile Pic*/}
-            <div className='w-3/12 phone:w-8/12 phone:mx-auto phone:my-16 flex flex-col mr-5'>
-              <div className='flex flex-col w-11/12 rounded-full border-2 border-red-700'>
-                    <img src="/personalContent/foto-perfil.webp" className="rounded-full"/>
+            <div className='w-3/12 phone:w-8/12 phone:mx-auto phone:my-16 flex justify-center flex-col mr-5'>
+              <div className='flex flex-col w-11/12 shadow-2xl shadow-red-700 rounded-full border-2 border-red-700'>
+                    <img src="/personalContent/foto-perfil.webp" className="rounded-full mx-auto"/>
+              </div>
+              <div className='w-11/12 flex justify-center'>
+                <a href='https://drive.google.com/file/d/1RSjYSsv-1pQxR8Q8vhgQO5Kf9UDfVSoS/view?usp=sharing'
+                className='w-5/12 flex justify-center border border-red-700 rounded-full transition-all shadow-2xl shadow-red-700 duration-500 px-15 py-1 bg-red-700 hover:bg-gray-900 hover:border-white active:bg-gray-900 active:border-white relative bottom-7 cursor-pointer'>
+                  <svg fill='#ffffffff' width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M22 0h-20v24h14l6-6v-18zm-6 18h4.36l-4.36 4.385v-4.385zm-3 1h-8v1h8v-1zm0-3h-8v1h8v-1zm6-2v-1h-14v1h14zm-7.059-4.968c-1.147-.265-2.214-.497-1.697-1.473 1.573-2.97.417-4.559-1.244-4.559-1.694 0-2.821 1.65-1.244 4.559.532.982-.575 1.214-1.697 1.473-1.024.237-1.062.745-1.059 1.635l.001.333h7.997l.001-.323c.004-.896-.03-1.407-1.058-1.645zm7.059.968h-4v1h4v-1zm0-2v-1h-4v1h4zm0-4h-4v1h4v-1z"/></svg>
+                </a>
               </div>
             </div>
         </div>
